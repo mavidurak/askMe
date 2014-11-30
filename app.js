8a4518f94e48b820d0358eeb2fe254a64cde5a1b
Furkan Başaran <frknbasaran@gmail.com>
2014-11-30 17:01:24 +0200

    /signup POST and insert method created

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ var app = express();
 
 mongoose.connect("mongodb://localhost:27017/appDB");
 
+var mainRouter = require('./routes/index');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.post('/signup', function(req, res) {
+    mainRouter.register(req.body.user);
+});
 
 app.listen(90);
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,18 @@
-routes will be here
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var User = require('../models/User');
+
+
+exports.register = function(body) {
+    
+    var userModel = mongoose.model('User',User);
+    var userObject = new User();
+    
+    User.username = body.username;
+    User.password = body.password;
+    
+    User.save(function(err){});
+}
+
+module.exports = router;
