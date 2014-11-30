2df42f6ee9ae9d197042f5b74feec6cfdef88a46
Onur Altunsoy <onuraltunsoy28@gmail.com>
2014-11-30 14:31:16 +0200

    Temel yapı hazırlandı.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -56,5 +56,5 @@ app.use(function(err, req, res, next) {
     });
 });
 
-
+app.listen(3001, 'localhost');
 module.exports = app;
