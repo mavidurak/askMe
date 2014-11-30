968f143fe7fba3f1240b41621c3eb271adc1f190
Onur Altunsoy <onuraltunsoy28@gmail.com>
2014-11-30 14:46:50 +0200

    Express-ejs-layout kütüphanesi projeye dahil edildi.

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var expressLayouts = require('express-ejs-layouts');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout', 'layout');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -21,6 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
 
 app.use('/', routes);
 app.use('/users', users);
