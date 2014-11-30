f1617e3d2558e9ebbcbda3f1d7ecc4c849dba107
emrekose26 <emre.kose2@ogr.sakarya.edu.tr>
2014-11-30 15:43:50 +0200

    Revert "new"
    This reverts commit 9d7280e4dd3ca2b5d85873e153fa8740261f3c5f.

--- app.js
+++ app.js
@@ -1,32 +1,74 @@
+/*
+  Uygulama için gerekli moduller
+*/
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var mongoose = require('mongoose');
 
+/**
+* API keys and Passport configuration.
+*/
+var secrets = require('./config/secrets');
+var passportConf = require('./config/passport');
+
+// mongo db baglantı ayarları
+mongoose.connect(secrets.db);
+mongoose.connection.on('error', function() {
+  console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
+});
+
+/*
+  uygulama url yönlendirmeleir
+*/
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// express uygulaması
 var app = express();
 
+// template motoru konfigrasyonu
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uygulama konfgirasyonları
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// url lerin route'lara yönlendirilmesis
+app.use('/', routes);
+app.use('/users', users);
 
-// kaydol
-
-// put, delete, post, get
-
-// put insert ekleme
-// post update guncelleme
-// delete silme
-// get verinin okunmas readonly
-
-app.get('/', function(req, res) {
-  res.json({"version":"1.0.0","description":"mavidurak restful web api"});
+// 404 hatası hazırlanması
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.put('/signup', function(req, res) {
-  res.json({
-    "username":req.body.username,
-    "password":req.body.password
-  });
-});
+// error handlers
 
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// uygulama ayağa kalktığında localhost:90 üzeirnden yayında olacak
 app.listen(90);
+
+// uygulamanın export edilmesi
+module.exports = app;
