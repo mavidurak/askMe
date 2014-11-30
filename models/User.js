ea71c6d1d79af6c3e67b29840097ba6af98b2ced
Fatma Koç <fatmakoc94@gmail.com>
2014-11-30 16:16:33 +0200

    User modeli oluşturuldu

--- models/User.js
+++ models/User.js
@@ -2,6 +2,17 @@ var mongoose = require('mongoose');
 
 var User = new mongoose.Schema({
 
+    username:String,
+    email: String,
+    password: String,
+    registeredAt: Date,
+    lastLogin: Date,
+    point: Number
+   
+   
+
 });
 
 exports.User = User;
+
+
