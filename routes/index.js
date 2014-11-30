7f30c69179bf4d188094041fabee8d34df0709c4
Onur Altunsoy <onuraltunsoy28@gmail.com>
2014-11-30 15:04:27 +0200

    Temel yapı oluşturuldu.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/login', function(req, res) {
+  res.render('login');
+});
+
+router.get('/signup', function(req, res) {
+  res.render('signup');
+});
+
 
 module.exports = router;
