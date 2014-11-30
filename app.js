85741f4c55dce0fe37a794fa1d1c870678da599a
co3moz <co3moz@gmail.com>
2014-11-30 16:58:14 +0200

    Çıkan hata çözüldü

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var app = express();
+var configs = require('./configs');
 
-mongoose.connect("mongodb://localhost:27017/appDB");
+mongoose.connect(configs.db);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.listen(90);
+app.listen(configs.port);
+console.log("Acildi");
--- configs.js
+++ configs.js
@@ -1,1 +1,2 @@
 exports.db = "mongodb://10.7.81.41/appDB";
+exports.port = 101;
