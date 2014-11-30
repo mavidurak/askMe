bce5bd2cbc0aa9fad9b276393b532f6341b96ca0
Furkan Başaran <frknbasaran@gmail.com>
2014-11-30 14:43:26 +0200

    app architect created
     asdasdasd

--- app.js
+++ app.js
@@ -1,31 +1,52 @@
+/*
+  Uygulama için gerekli moduller
+*/
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var mongoose = require('mongoose');
 
+/**
+* API keys and Passport configuration.
+*/
+var secrets = require('./config/secrets');
+var passportConf = require('./config/passport');
+
+// mongo db baglantı ayarları
+mongoose.connect(secrets.db);
+mongoose.connection.on('error', function() {
+  console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
+});
+
+/*
+  uygulama url yönlendirmeleir
+*/
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+// express uygulaması
 var app = express();
 
-// view engine setup
+// template motoru konfigrasyonu
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// uygulama konfgirasyonları
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// url lerin route'lara yönlendirilmesis
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+// 404 hatası hazırlanması
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
@@ -46,15 +67,8 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+// uygulama ayağa kalktığında localhost:90 üzeirnden yayında olacak
+app.listen(90);
 
-
+// uygulamanın export edilmesi
 module.exports = app;
