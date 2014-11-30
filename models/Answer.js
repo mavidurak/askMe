050d9337a2b0cfa6920679633c118a16b7b9c79e
co3moz <co3moz@gmail.com>
2014-11-30 16:54:33 +0200

    Tab fix

--- models/Answer.js
+++ models/Answer.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 
 var Answer = new Schema({
-    question :  { type: ObjectId, ref: 'Question' },
-    text: String,
-    createdBy : { type: ObjectId, ref: 'User' },
-    createdAt : Date,
-    comments: [{
-        createdBy: { type: ObjectId, ref: 'User' },
-        createdAt: Date,
-        text: String,
-    }}
+	question :  { type: ObjectId, ref: 'Question' },
+	text: String,
+	createdBy : { type: ObjectId, ref: 'User' },
+	createdAt : Date,
+	comments: [{
+		createdBy: { type: ObjectId, ref: 'User' },
+		createdAt: Date,
+		text: String,
+	}}
 });
 
 module.export = Answer;
--- models/Questions.js
+++ models/Questions.js
@@ -1,17 +1,13 @@
 var mongoose = require('mongoose');
 
 var QuestionsShema = new mongoose.Schema({
-
-    header:String,
-    description:String,
-    askedBy: { type: ObjectId, ref: 'UserSchema' },
-    askedAt:Date,
-    tags: [],
-    votes: [],
-    lastUpdated:Date
-   
+	header:String,
+	description:String,
+	askedBy: { type: ObjectId, ref: 'UserSchema' },
+	askedAt:Date,
+	tags: [],
+	votes: [],
+	lastUpdated:Date
 });
 
 exports.Questions = Questions;
-
-
