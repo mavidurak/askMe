130c4e53f1ba5ed1661d03df05a38cfb4a6c66d9
Furkan Başaran <frknbasaran@gmail.com>
2014-12-02 17:15:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressLayouts);
 
 app.get('/', function(req, res){
-    res.render('index');
+    res.render('index',{title:'Main Page - askMe'});
 });
 
 app.listen(configs.port);
